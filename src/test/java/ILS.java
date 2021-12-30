e778dbd700c964a835139b2a1f13a761adba65e7
shimonss <96369094+shimonss@users.noreply.github.com>
2021-12-30 09:39:45 +0200

    Delete ILS.java

--- src/test/java/ILS.java
+++
@@ -1,9 +0,0 @@
-public class ILS extends coin {
-    private final double value = 0.28;
-    public double getValue() {
-        return value;
-    }
-    public double calculate(double input){
-        return input*getValue();
-    }
-}
